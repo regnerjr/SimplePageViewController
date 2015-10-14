a2270ed048461b78b843242e1f9c73b84f13098d
John Regner <regnerjr@gmail.com>
2015-10-14 09:25:34 -0700

    Initial Commit

---
+++ SimplePageViewController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimplePageViewController
+//
+//  Created by John Regner on 10/14/15.
+//  Copyright © 2015 johnregner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
